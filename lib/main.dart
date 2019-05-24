5e0dd5e7fd36a42d0532b2ad7f718d1ee8d22bb7
Phuree Kanusont <60070075@kmitl.ac.th>
2019-05-24 21:17:10 +0700

    Change hex color to swatch

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,28 @@ import './Home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  static Map<int, Color> color = {
+    50: Color.fromRGBO(136, 14, 79, .1),
+    100: Color.fromRGBO(136, 14, 79, .2),
+    200: Color.fromRGBO(136, 14, 79, .3),
+    300: Color.fromRGBO(136, 14, 79, .4),
+    400: Color.fromRGBO(136, 14, 79, .5),
+    500: Color.fromRGBO(136, 14, 79, .6),
+    600: Color.fromRGBO(136, 14, 79, .7),
+    700: Color.fromRGBO(136, 14, 79, .8),
+    800: Color.fromRGBO(136, 14, 79, .9),
+    900: Color.fromRGBO(136, 14, 79, 1),
+  };
+
+  MaterialColor colorCustom = MaterialColor(0xFF880E4F, color);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: colorCustom,
       ),
       home: Home(),
     );
